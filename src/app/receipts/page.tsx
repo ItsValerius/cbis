9ec0c7de9cfb5367f96393a6a83707e798fda3f3
Valentin Kueck <kueck.valentin@googlemail.com>
2023-12-12 11:38:59 +0100

    btn text

--- src/app/receipts/page.tsx
+++ src/app/receipts/page.tsx
@@ -3,17 +3,34 @@ import { postReceipt } from "./action";
 export default async function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-     <form action={postReceipt} className="flex justify-center items-center flex-col gap-2">
-     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="file_input">Upload receipt</label>
+      <form
+        action={postReceipt}
+        className="flex flex-col items-center justify-center gap-2"
+      >
+        <label
+          className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+          htmlFor="file_input"
+        >
+          Upload receipt
+        </label>
 
-     <input id="file_input" className="block w-full text-sm text-slate-500
-        file:mr-4 file:py-2 file:px-4 file:rounded-md
-        file:border-0 file:text-sm file:font-semibold
-        file:bg-pink-50 file:text-blue-700
-        hover:file:bg-blue-100" type="file" name="receipt"></input>
-     <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
-
-     </form>
+        <input
+          id="file_input"
+          className="block w-full text-sm text-slate-500
+        file:mr-4 file:rounded-md file:border-0 file:bg-pink-50
+        file:px-4 file:py-2 file:text-sm
+        file:font-semibold file:text-blue-700
+        hover:file:bg-blue-100"
+          type="file"
+          name="receipt"
+        ></input>
+        <button
+          type="submit"
+          className="mb-2 me-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Submit
+        </button>
+      </form>
     </main>
   );
 }
