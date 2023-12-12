1438a1a4fa21c32b39a62d7e8599820a268e7830
Valentin Kueck <kueck.valentin@googlemail.com>
2023-12-12 18:04:28 +0100

    log

--- src/app/receipts/action.ts
+++ src/app/receipts/action.ts
@@ -5,6 +5,7 @@ import { env } from "~/env";
 export async function postReceipt(formData: FormData) {
   const receipt = formData.get("receipt") as File;
   if (receipt.size === 0) return;
+  console.log(formData);
 
   await fetch(env.RECEIPT_PROCESSOR_URL, { method: "POST", body: formData });
 }
