88c4da1b03c8fe017a47d079c981d0aa19a6a443
Marvin Zeitschner <m.zeitschner@gmail.com>
2023-12-17 22:05:44 +0100

    feat: pg docker setup

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,16 +1,16 @@
 import { env } from "~/env";
 import * as schema from "./schema";
-import { Pool, neonConfig } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-serverless";
+import { Pool } from "pg";
+import { drizzle } from "drizzle-orm/node-postgres";
 
 import ws from "ws";
-neonConfig.webSocketConstructor = ws;
+// neonConfig.webSocketConstructor = ws;
 
-neonConfig.fetchConnectionCache = true;
+// neonConfig.fetchConnectionCache = true;
 const pool = new Pool({ connectionString: env.DATABASE_URL });
-pool.on("error", (err) => console.error(err)); // deal with e.g. re-connect
-// ...
+// pool.on("error", (err) => console.error(err)); // deal with e.g. re-connect
+// // ...
 
-const client = await pool.connect();
+// const client = await pool.connect();
 
-export const db = drizzle(client, { schema });
+export const db = drizzle(pool, { schema });
