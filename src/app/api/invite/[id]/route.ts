2c4ad3ca00271014289e036e0fe1c831455d6456
Marvin Zeitschner <85627862+MarvinZeitschner@users.noreply.github.com>
2023-12-25 19:06:58 +0100

    feat: register form

--- src/app/api/invite/[id]/route.ts
+++ src/app/api/invite/[id]/route.ts
@@ -1,16 +1,34 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { NextResponse } from "next/server";
+import { eq } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
 import { auth } from "~/lib/auth";
+import { db } from "~/server/db";
+import { groups, usersToGroups } from "~/server/db/schema";
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-  const query = req.query;
-  const { id } = query;
-
-  if (!id || Array.isArray(id)) return;
-
+export async function POST(
+  req: NextRequest,
+  { params }: { params: { id: string } },
+) {
   const session = await auth();
-  console.log("session", session);
-  console.log("id", id);
+  if (!session?.user) {
+    return new NextResponse("Unauthorized", {
+      status: 401,
+    });
+  }
+
+  const group = await db.query.groups.findFirst({
+    where: eq(groups.inviteUuid, params.id),
+  });
+
+  if (!group) {
+    return new NextResponse("Group Not Found", {
+      status: 404,
+    });
+  }
+
+  await db.insert(usersToGroups).values({
+    groupId: group.id,
+    userId: session!.user.id,
+  });
 
   return new NextResponse();
 }
