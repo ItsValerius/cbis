39b0702b949630c8507ead9e2017e694e55db297
Marvin Zeitschner <m.zeitschner@gmail.com>
2024-01-05 15:47:44 +0100

    fix: landing page not protected

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { withAuth } from "next-auth/middleware";
 export default withAuth({
   callbacks: {
     authorized({ req, token }) {
-      const notProtected = ["/api/receipts"];
+      const notProtected = ["/api/receipts", "/"];
       if (notProtected.includes(req.nextUrl.pathname)) return true;
 
       if (token) return true;
