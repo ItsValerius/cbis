d5caa3a2d699c91c46358dbbda2c106e9b7619ba
Valentin Kueck <kueck.valentin@googlemail.com>
2024-01-05 15:56:30 +0100

    update env

--- src/env.js
+++ src/env.js
@@ -29,7 +29,13 @@ export const env = createEnv({
       // VERCEL_URL doesn't include `https` so it cant be validated as a URL
       process.env.VERCEL ? z.string() : z.string().url(),
     ),
-    FRONTEND_URL: z.string().url().nullable(),
+    FRONTEND_URL: z.preprocess(
+      // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
+      // Since NextAuth.js automatically uses the VERCEL_URL if present.
+      (str) => process.env.VERCEL_URL ?? str,
+      // VERCEL_URL doesn't include `https` so it cant be validated as a URL
+      process.env.VERCEL ? z.string() : z.string().url(),
+    ),
   },
 
   /**
