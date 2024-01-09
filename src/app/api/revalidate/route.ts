6a7a7a6a15241c92ab7da609dc329e38aeb7149f
Valentin Kueck <kueck.valentin@googlemail.com>
2024-01-09 20:54:23 +0100

    caching

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,7 +1,7 @@
 import { revalidatePath } from "next/cache";
 
 export async function GET() {
-  revalidatePath(`/(user)/groups/[id]`, "page");
+  revalidatePath(`/(user)/groups/[id]`);
 
   return new Response();
 }
