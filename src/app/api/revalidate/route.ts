39dea3fd02528cee22069046d1f46cdc0ad65371
Valentin Kueck <kueck.valentin@googlemail.com>
2024-01-09 20:50:47 +0100

    try https://nextjs.org/docs/app/api-reference/functions/revalidatePath\#revalidating-a-page-path

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,9 +1,7 @@
 import { revalidatePath } from "next/cache";
 
 export async function GET() {
-  revalidatePath(`/(user)/groups/[id]/page`, "page");
-  revalidatePath(`/(user)/groups/[id]`);
-  revalidatePath(`/(user)/groups`);
-  revalidatePath("/");
+  revalidatePath(`/(user)/groups/[id]`, "page");
+
   return new Response();
 }
