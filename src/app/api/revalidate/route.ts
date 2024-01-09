7b01970d062826d330e3d623411a7d62b5c40ec5
Valentin Kueck <kueck.valentin@googlemail.com>
2024-01-09 20:29:39 +0100

    try and fix revalidation

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,8 +1,9 @@
 import { revalidatePath } from "next/cache";
 
 export async function GET() {
-  revalidatePath("/receipts/list", "page");
-  revalidatePath("/receipts");
+  revalidatePath(`/groups/[id]/page`, "page");
+  revalidatePath(`/groups/[id]`);
+  revalidatePath(`/groups`);
   revalidatePath("/");
   return new Response();
 }
